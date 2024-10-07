66018e3d6069d0f72436ee48c138981da470248a
unknown <kolesnik0vplaton@yandex.ru>
2024-10-07 18:22:12 +0300

    Первый пуш квиза

--- logic.py
+++ logic.py
@@ -1,5 +1,5 @@
 # Задание 2 - Импортируй нужные классы
-
+from telebot.types import InlineKeyboardMarkup , InlineKeyboardButton
 class Question:
 
     def __init__(self, text, answer_id, *options):
@@ -8,9 +8,20 @@ class Question:
         self.options = options
 
     # Задание 1 - Создай геттер для получения текста вопроса
+    @property 
+    def text(self):
+        return self.__text
     
     def gen_markup(self):
         # Задание 3 - Создай метод для генерации Inline клавиатуры
+        markup = InlineKeyboardMarkup()
+        markup.row_width(len(self.options))
+        for i,  option in enumerate(self.options):
+            if i == self.__answer_id:
+                markup.add(InlineKeyboardButton(option, callback_data='correct'))
+            else:
+                markup.add(InlineKeyboardButton(option, callback_data='wrong'))
+
         return markup
 
 # Задание 4 - заполни список своими вопросами
--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-
+from collections import defaultdict
 import telebot
 from config import token
 # Задание 7 - испортируй команду defaultdict
 from logic import quiz_questions
 
+bot = telebot.TeleBot(token)
+
 user_responses = {} 
 # Задание 8 - создай словарь points для сохранения количества очков пользователя
 
-bot = telebot.TeleBot(token)
+
+points = defaultdict(int)
+
 
 def send_question(chat_id):
     bot.send_message(chat_id, quiz_questions[user_responses[chat_id]].text, reply_markup=quiz_questions[user_responses[chat_id]].gen_markup())
@@ -17,14 +21,18 @@ def callback_query(call):
 
     if call.data == "correct":
         bot.answer_callback_query(call.id, "Answer is correct")
-        # Задание 9 - добавь очки пользователю за правильный ответ
     elif call.data == "wrong":
-        bot.answer_callback_query(call.id,  "Answer is wrong")
-      
+        bot.answer_callback_query(call.id,  "Answer is wrong")  
     # Задание 5 - реализуй счетчик вопросов
-
+     # Задание 9 - добавь очки пользователю за правильный ответ
     # Задание 6 - отправь пользователю сообщение с количеством его набранных очков, если он ответил на все вопросы, а иначе отправь следующий вопрос
-
+    user_responses[call.message.chat.id] += 1
+    if user_responses[call.message.chat.id] > 2:
+        bot.send_message(call.message.chat.id, f"The end!")
+    else:
+        send_question(call.message.chat.id)
+    print(points)
+        
 
 @bot.message_handler(commands=['start'])
 def start(message):
